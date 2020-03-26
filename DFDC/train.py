b8a52fff710e14d8209924ee488b9a1808218f64
Nizamuddin <nizamphoenix@gmail.com>
2020-03-26 23:41:12 +1100

    Create train.py

---
+++ DFDC/train.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os
+from tqdm import tqdm,trange
+from sklearn.model_selection import train_test_split
+import sklearn.metrics
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import warnings
+warnings.filterwarnings("ignore")
