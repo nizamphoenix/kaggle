d8daa91745dee395f884a0f66ab629e114282199
Nizamuddin <nizamphoenix@gmail.com>
2020-03-26 23:42:09 +1100

    Update train.py

--- DFDC/train.py
+++ DFDC/train.py
@@ -13,3 +13,44 @@ import torch.nn.functional as F
 
 import warnings
 warnings.filterwarnings("ignore")
+
+from torch.utils.data import Dataset, DataLoader
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+class ImageDataset(Dataset):
+    def __init__(self, X, y, training=True, transform=None):
+        self.X = X
+        self.y = y
+        self.transform = transform
+        self.training = training
+
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        img = self.X[idx]
+
+        if self.transform is not None:
+          res = self.transform(image=img)
+          img = res['image']
+        
+        img = np.rollaxis(img, 2, 0)
+        # img = np.array(img).astype(np.float32) / 255.
+
+        labels = self.y[idx]
+        labels = np.array(labels).astype(np.float32)
+        return [img, labels]
+val_y=[]
+for df_val,num in tqdm(zip(df_vals,val_nums),total=len(df_vals)):
+    images = list(df_val.columns.values)
+    for x in images:
+        try:
+            val_paths.append(get_path(num,x))
+            val_y.append(LABELS.index(df_val[x]['label']))
+        except Exception as err:
+            #print(err)
+            pass
