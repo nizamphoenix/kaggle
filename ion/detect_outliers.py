54eab2d3ac2df868cffbd467046d464a47dbae08
Nizamuddin <nizamphoenix@gmail.com>
2020-04-20 15:49:12 +1000

    Create detect_outliers.py

---
+++ ion/detect_outliers.py
@@ -0,0 +1,14 @@
+#REMOVING OUTLIERS: POINTS OUTSIDE 3SD
+temp = []
+for i in range(10): 
+    print('Processing Batch-{}'.format(i+1))
+    a = i * 500000
+    b = (i+1) * 500000
+    temp_df = df[a:b]
+    plt.plot(temp_df['open_channels'],temp_df['signal'])
+    plt.show()
+    temp_df = temp_df[np.abs(temp_df.signal-temp_df.signal.mean()) <= (2*temp_df.signal.std())]
+    print('Aftre removing outliers in Batch-{}'.format(i+1))
+    plt.plot(temp_df['open_channels'],temp_df['signal'])
+    plt.show()
+    temp.append(temp_df)
