5225110a5a118685dd1566d272507b53a8e8223a
Nizamuddin <nizamphoenix@gmail.com>
2020-04-30 19:08:32 +1000

    Create f1_macro.py

---
+++ ion/f1_macro.py
@@ -0,0 +1,18 @@
+#credits: https://www.kaggle.com/c/liverpool-ion-switching/discussion/132646
+
+import tensorflow as tf
+import keras.backend as K
+
+def f1_macro(y_true, y_pred):
+    y_pred = K.round(y_pred)
+    tp = K.sum(K.cast(y_true*y_pred, 'float'), axis=0)
+    # tn = K.sum(K.cast((1-y_true)*(1-y_pred), 'float'), axis=0)
+    fp = K.sum(K.cast((1-y_true)*y_pred, 'float'), axis=0)
+    fn = K.sum(K.cast(y_true*(1-y_pred), 'float'), axis=0)
+
+    p = tp / (tp + fp + K.epsilon())
+    r = tp / (tp + fn + K.epsilon())
+
+    f1 = 2*p*r / (p+r+K.epsilon())
+    f1 = tf.where(tf.math.is_nan(f1), tf.zeros_like(f1), f1)
+    return K.mean(f1)
