493996ee709c842af3f21267a570944122680a2b
Nizamuddin <nizamphoenix@gmail.com>
2020-04-30 19:09:14 +1000

    callback added

--- ion/f1_macro.py
+++ ion/f1_macro.py
@@ -16,3 +16,30 @@ def f1_macro(y_true, y_pred):
     f1 = 2*p*r / (p+r+K.epsilon())
     f1 = tf.where(tf.math.is_nan(f1), tf.zeros_like(f1), f1)
     return K.mean(f1)
+
+
+class CustomCallback(tf.keras.callbacks.Callback):
+    def __init__(self, training_data, validation_data):
+        self.x = training_data[0]
+        self.y = training_data[1]
+        self.x_val = validation_data[0]
+        self.y_val = validation_data[1]
+        
+    def on_train_begin(self, logs={}):
+        return
+
+    def on_train_end(self, logs={}):
+        return
+
+    def on_epoch_begin(self, epoch, logs={}):
+        return
+
+    def on_batch_begin(self, batch, logs={}):
+        return
+
+    def on_batch_end(self, batch, logs={}):
+        return
+
+    def on_epoch_end(self, epoch, logs={}):
+        y_pred_val = self.model.predict(self.x_val)
+        return f1_macro(self.y_val,y_pred_val)
