e1c5943b486d6e04948c0fde0c536a4bd87431d8
Nizamuddin <nizamphoenix@gmail.com>
2020-05-06 22:11:34 +1000

    Update attention_layer.py

--- ion/attention_layer.py
+++ ion/attention_layer.py
@@ -57,4 +57,37 @@ class Attention(Layer):
             length = tf.shape(x)[2]
             x = tf.transpose(x, [0, 2, 1, 3])  # --> [batch, length, num_heads, depth]
             return tf.reshape(x, [batch_size, length, self.hidden_size])        
+        def call(self, inputs):
+        """Apply attention mechanism to inputs.
+        Args:
+          inputs: a tensor with shape [batch_size, length_x, hidden_size]
+        Returns:
+          Attention layer output with shape [batch_size, length_x, hidden_size]
+        """
+    
+        q = self.dense(inputs)
+        k = self.dense(inputs)
+        v = self.dense(inputs)
 
+        q = self.split_heads(q)
+        k = self.split_heads(k)
+        v = self.split_heads(v)
+        
+        # Scale q to prevent the dot product between q and k from growing too large.
+        depth = (self.hidden_size // self.num_heads)
+        q *= depth ** -0.5
+        
+        logits = tf.matmul(q, k, transpose_b=True)
+        # logits += self.bias
+        weights = tf.nn.softmax(logits, name="attention_weights")
+        
+        if self.trainable:
+            weights = tf.nn.dropout(weights, 1.0 - self.attention_dropout)
+        
+        attention_output = tf.matmul(weights, v)
+        attention_output = self.combine_heads(attention_output)
+        attention_output = self.dense(attention_output)
+        return attention_output
+        
+    def compute_output_shape(self, input_shape):
+        return tf.TensorShape(input_shape)
