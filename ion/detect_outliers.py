2887ba591573b7c48288e0bda3a93eb56f4b2810
Nizamuddin <nizamphoenix@gmail.com>
2020-06-04 20:57:56 +1000

    Update detect_outliers.py

--- ion/detect_outliers.py
+++ ion/detect_outliers.py
@@ -1,14 +1,16 @@
 #REMOVING OUTLIERS: removing data points not within 2-Standard deviations
-temp = []
-for i in range(10): 
-    print('Processing Batch-{}'.format(i+1))
-    a = i * 500000
-    b = (i+1) * 500000
-    temp_df = df[a:b]
-    plt.plot(temp_df['open_channels'],temp_df['signal'])
-    plt.show()
-    temp_df = temp_df[np.abs(temp_df.signal-temp_df.signal.mean()) <= (2*temp_df.signal.std())]
-    print('Aftre removing outliers in Batch-{}'.format(i+1))
-    plt.plot(temp_df['open_channels'],temp_df['signal'])
-    plt.show()
-    temp.append(temp_df)
+def remove_outliers(df):
+    temp = []
+    for i in range(10): 
+        print('Processing Batch-{}'.format(i+1))
+        a = i * 500000
+        b = (i+1) * 500000
+        temp_df = df[a:b]
+        plt.plot(temp_df['open_channels'],temp_df['signal'])
+        plt.show()
+        temp_df = temp_df[np.abs(temp_df.signal-temp_df.signal.mean()) <= (2*temp_df.signal.std())]
+        print('Aftre removing outliers in Batch-{}'.format(i+1))
+        plt.plot(temp_df['open_channels'],temp_df['signal'])
+        plt.show()
+        temp.append(temp_df)
+        return temp
