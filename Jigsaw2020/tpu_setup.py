f4b4ef8890f3f160ed018d9ffb4729fd5f371a51
Nizamuddin <nizamphoenix@gmail.com>
2020-06-06 21:36:51 +1000

    Create tpu_setup.py

---
+++ Jigsaw2020/tpu_setup.py
@@ -0,0 +1,20 @@
+def setup_tpu():
+    # Detect hardware, return appropriate distribution strategy
+    try:
+        # TPU detection. No parameters necessary if TPU_NAME environment variable is
+        # set: this is always the case on Kaggle.
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+        print('Running on TPU ', tpu.master())
+    except ValueError:
+        tpu = None
+
+    if tpu:
+        tf.config.experimental_connect_to_cluster(tpu)
+        tf.tpu.experimental.initialize_tpu_system(tpu)
+        strategy = tf.distribute.experimental.TPUStrategy(tpu)
+    else:
+        # Default distribution strategy in Tensorflow. Works on CPU and single GPU.
+        strategy = tf.distribute.get_strategy()
+
+    print("REPLICAS: ", strategy.num_replicas_in_sync)
+    return strategy
