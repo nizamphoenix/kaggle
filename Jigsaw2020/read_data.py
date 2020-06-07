906eab8eae62abc34ad8536e7b6c26cad2171d21
Nizamuddin <nizamphoenix@gmail.com>
2020-06-07 23:34:25 +1000

    Update read_data.py

--- Jigsaw2020/read_data.py
+++ Jigsaw2020/read_data.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 def read_data():
     train1 = pd.read_csv("/kaggle/input/jigsaw-multilingual-toxic-comment-classification/jigsaw-toxic-comment-train.csv", usecols=["comment_text", "toxic"])
-    train2 = pd.read_csv("/kaggle/input/jigsaw-multilingual-toxic-comment-classification/jigsaw-unintended-bias-train.csv", usecols=["comment_text", "toxic"])
-    train2.toxic = train2.toxic.round().astype(int)
+   
 
     df_valid = pd.read_csv('/kaggle/input/jigsaw-multilingual-toxic-comment-classification/validation.csv')
     test = pd.read_csv('/kaggle/input/jigsaw-multilingual-toxic-comment-classification/test.csv')
