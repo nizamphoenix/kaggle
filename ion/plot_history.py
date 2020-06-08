bee42ec605c17cf0e0dc3475c3c6aa1ff735ee0e
Nizamuddin <nizamphoenix@gmail.com>
2020-06-08 20:57:45 +1000

    Create plot_history.py

---
+++ ion/plot_history.py
@@ -0,0 +1,39 @@
+import matplotlib.image  as mpimg
+import matplotlib.pyplot as plt
+
+#-----------------------------------------------------------
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+#-----------------------------------------------------------
+mae=history.history['mae']
+loss=history.history['loss']
+
+epochs=range(len(loss)) # Get number of epochs
+
+#------------------------------------------------
+# Plot MAE and Loss
+#------------------------------------------------
+plt.plot(epochs, mae, 'r')
+plt.plot(epochs, loss, 'b')
+plt.title('MAE and Loss')
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend(["MAE", "Loss"])
+
+plt.figure()
+
+epochs_zoom = epochs[200:]
+mae_zoom = mae[200:]
+loss_zoom = loss[200:]
+
+#------------------------------------------------
+# Plot Zoomed MAE and Loss
+#------------------------------------------------
+plt.plot(epochs_zoom, mae_zoom, 'r')
+plt.plot(epochs_zoom, loss_zoom, 'b')
+plt.title('MAE and Loss')
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend(["MAE", "Loss"])
+
+plt.figure()
