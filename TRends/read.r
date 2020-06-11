9ecb5f1d03f3693cea6d441b9fa3d168c2fd13ea
Nizamuddin <nizamphoenix@gmail.com>
2020-06-11 19:34:13 +1000

    Update read.r

--- TRends/read.r
+++ TRends/read.r
@@ -1,12 +1,7 @@
 get.data <- function(fnc.path,loading.path,target.path) {
-fnc.path = "../input/trends-assessment-prediction/fnc.csv"
-loading.path = "../input/trends-assessment-prediction/loading.csv"
-target.path = "../input/trends-assessment-prediction/train_scores.csv"
-
 fnc_df = read.csv(fnc.path)
 loading_df = read.csv(loading.path)
 labels_df = read.csv(target.path)
-
 library(tidyr)#drop_na()
 df <- merge(fnc_df,loading_df,by="Id")
 labels_df$is.train  = T 
@@ -14,17 +9,17 @@ df <- merge(x=labels_df, y=df, all.x=TRUE)
 df<-drop_na(df)
 df<-df[,-c(1,7)]
 head(df)
-
-smp_size <- floor(0.75 * nrow(df))
-
-## set the seed to make your partition reproducible
-set.seed(123)
-train_ind <- sample(seq_len(nrow(df)), size = smp_size)
-
-train <- df[train_ind, ]
-test <- df[-train_ind, ]
-dim(train)
-dim(test)
-  return(train,test)
+return(df)
 }
 
+
+data<-get.data("../input/trends-assessment-prediction/fnc.csv","../input/trends-assessment-prediction/loading.csv","../input/trends-assessment-prediction/train_scores.csv")
+
+smp_size <- floor(0.75 * nrow(data))
+## set the seed to make your partition reproducible
+set.seed(123)
+train_ind <- sample(seq_len(nrow(data)), size = smp_size)
+train <- data[train_ind, ]
+test <- data[-train_ind, ]
+dim(train)
+dim(test)
