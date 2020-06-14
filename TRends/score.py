a6b8be03e3850e36b54912578400a4d3fac8043d
Nizamuddin <nizamphoenix@gmail.com>
2020-06-15 08:12:39 +1000

    Update score.py

--- TRends/score.py
+++ TRends/score.py
@@ -2,5 +2,5 @@ import numpy as np
 def trends_score(y_true,y_preds):
     y_true = np.array(y_true)
     y_preds = np.array(y_preds)
-    w = np.array([.3, .175, .175, .175, .175]).reshape(-1,1)
+    w = np.array([.3, .175, .175, .175, .175])
     return np.mean(np.matmul(np.abs(y_true-y_preds),w/np.mean(y_true,axis=0)),axis=0)
