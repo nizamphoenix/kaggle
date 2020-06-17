f1047b82b78222cd99b3b363e2bc982fe5bd9d39
Nizamuddin <nizamphoenix@gmail.com>
2020-06-17 21:20:06 +1000

    Update read_data.py

--- TRends/read_data.py
+++ TRends/read_data.py
@@ -1,19 +1,21 @@
-import cudf#rapid ai
-def get_data():
-  '''
-  returns train dataframe,test dataframe,test dataframe from site2(note:not all are mentioned)
-  '''
-  fnc_df = cudf.read_csv("../input/trends-assessment-prediction/fnc.csv")
-  loading_df = cudf.read_csv("../input/trends-assessment-prediction/loading.csv")
-  fnc_features, loading_features = list(fnc_df.columns[1:]), list(loading_df.columns[1:])
-  df = fnc_df.merge(loading_df, on="Id")
-  target_df = cudf.read_csv("../input/trends-assessment-prediction/train_scores.csv")
-  target_df["is_train"] = 1
-  df = df.merge(target_df, on="Id", how="left")
-  test_df = df[df["is_train"] != 1].copy()
-  df = df[df["is_train"] == 1].copy()
-  print("training shape={0} | testing shape={1}".format(df.shape, test_df.shape))
-  #510 Ids of site2 are known, there are more 510 is not all. training data does not have site2 Ids
-  site2_df = cudf.read_csv("../input/trends-assessment-prediction/reveal_ID_site2.csv")
-  testdf_site2 = test_df[test_df['Id'].isin(list(site2_df['Id']))]
-  return df,test_df,testdf_site2
+import os
+import pandas as pd
+import cudf#rapidsai--to run on GPU
+def get_train_test(fnc_file,loadings_file,lablels_file):
+    '''
+    function to get training and test data sets
+    '''
+    path = "../input/trends-assessment-prediction/"
+    fnc_df = pd.read_csv(os.path.join(path,fnc_file))
+    loading_df = pd.read_csv(os.path.join(path,loadings_file))
+    fnc_features, loading_features = list(fnc_df.columns[1:]), list(loading_df.columns[1:])
+    df = fnc_df.merge(loading_df, on="Id")
+    labels_df = pd.read_csv(os.path.join(path,lablels_file))
+    labels_df["is_train"] = True
+    df = df.merge(labels_df, on="Id", how="left")
+    test_df = df[df["is_train"] != True].copy()
+    df = df[df["is_train"] == True].copy()
+    # Giving less importance to FNC features since they are easier to overfit due to high dimensionality.
+    df[fnc_features] *= 1/600
+    test_df[fnc_features] *= 1/600
+    return df,test_df,fnc_features, loading_features
