eeae2ac143f3156ab3c0968f3b9d32f812044a76
Nizamuddin <nizamphoenix@gmail.com>
2020-06-19 22:20:30 +1000

    Update encode.py

--- Jigsaw2020/encode.py
+++ Jigsaw2020/encode.py
@@ -1,3 +1,4 @@
+%%time
 from joblib import Parallel, delayed
 from transformers import AutoTokenizer
 
@@ -24,3 +25,18 @@ def regular_encode(row, is_test=False, maxlen=MAX_LEN):
         )
         outp = np.array(enc_di['input_ids']), row[1]
     return outp
+                        
+rows = zip(df_train['comment_text'].values.tolist(), df_train.toxic.values.tolist())
+train = Parallel(n_jobs=4, backend='multiprocessing')(delayed(regular_encode)(row) for row in tqdm(rows))
+
+rows = zip(df_valid['comment_text'].values.tolist(), df_valid.toxic.values.tolist())
+valid = Parallel(n_jobs=4, backend='multiprocessing')(delayed(regular_encode)(row) for row in tqdm(rows))
+                        
+rows = test.content.values.tolist()
+x_test = Parallel(n_jobs=4, backend='multiprocessing')(delayed(regular_encode)(row,is_test=True) for row in tqdm(rows))
+
+x_train = np.vstack(np.array(train)[:,0])
+y_train = np.array(train)[:,1].astype(np.int32)
+x_valid = np.vstack(np.array(valid)[:,0])
+y_valid = np.array(valid)[:,1].astype(np.int32)
+x_train.shape,y_train.shape,x_valid.shape,y_valid.shape
