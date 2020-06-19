6a4a0070ac0950c68a011c35861a6ed54c5f10d9
Nizamuddin <nizamphoenix@gmail.com>
2020-06-19 22:17:43 +1000

    Update create_model.py

--- Jigsaw2020/create_model.py
+++ Jigsaw2020/create_model.py
@@ -1,15 +1,5 @@
-import os
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-from tensorflow.keras.layers import Dense, Input, Dropout, BatchNormalization, Activation
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.models import Model
-from tensorflow.keras.callbacks import ModelCheckpoint
-
-
-
 from keras.utils.generic_utils import get_custom_objects
+from tensorflow.keras.layers import Activation
 def gelu(x):
     """Gaussian Error Linear Unit.
     This is a smoother version of the RELU.
@@ -22,18 +12,3 @@ def gelu(x):
     cdf = 0.5 * (1.0 + tf.tanh((np.sqrt(2 / np.pi) * (x + 0.044715 * tf.pow(x, 3)))))
     return x * cdf#`x` with the GELU activation applied
 get_custom_objects().update({'gelu': Activation(gelu)})
-
-def build_model(transformer, max_len=512):
-    """
-    https://www.kaggle.com/xhlulu/jigsaw-tpu-distilbert-with-huggingface-and-keras
-    """
-    input_word_ids = Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
-    sequence_output = transformer(input_word_ids)[0]
-    cls_token = sequence_output[:, 0, :]#cls_token is a vector of length 768 marginalised against other 2 dimensions
-    x = Dense(16, activation=gelu)(cls_token)
-    x = BatchNormalization()(x)
-    x = Dropout(0.3)(x)
-    out = Dense(1, activation='sigmoid')(x)
-    model = Model(inputs=input_word_ids, outputs=out)
-    model.compile(Adam(lr=1e-5), loss='binary_crossentropy', metrics=['accuracy'])
-    return model
