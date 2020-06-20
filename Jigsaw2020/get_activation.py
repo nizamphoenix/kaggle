5b462b902ef7ec223071edfc9ae5f5f8f37eccd7
Nizamuddin <nizamphoenix@gmail.com>
2020-06-20 22:25:52 +1000

    Create get_activation.py

---
+++ Jigsaw2020/get_activation.py
@@ -0,0 +1,14 @@
+from tensorflow.keras.layers import Activation
+def gelu(x):
+    """Gaussian Error Linear Unit.
+    This is a smoother version of the RELU.
+    Original paper: https://arxiv.org/abs/1606.08415
+    Args:
+        x: float Tensor to perform activation.
+    Returns:
+        `x` with the GELU activation applied.
+    """
+    cdf = 0.5 * (1.0 + tf.tanh((np.sqrt(2 / np.pi) * (x + 0.044715 * tf.pow(x, 3)))))
+    return x * cdf#`x` with the GELU activation applied
+from keras.utils.generic_utils import get_custom_objects
+get_custom_objects().update({'gelu': Activation(gelu)})
