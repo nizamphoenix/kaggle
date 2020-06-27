e7764ffceff565fba277ed5112c4d5ec63256001
Nizamuddin <nizamphoenix@gmail.com>
2020-06-27 19:13:05 +1000

    removed optimizer

--- TRends/tabnet_model.py
+++ TRends/tabnet_model.py
@@ -1,20 +1,18 @@
-def get_model(emb_szs,dls,n_features,loss):
+def get_model(emb_szs,dls,n_features):
     model=TabNetModel(
         emb_szs,
         n_cont=n_features,
         out_sz=5,
         embed_p=0.0,
         y_range=None,
-        n_d=10,
-        n_a=18,
+        n_d=32,
+        n_a=32,
         n_steps=2,
         gamma=1.194,
         n_independent=0,
         n_shared=4,
         epsilon=1e-15,
         virtual_batch_size=128,
-        momentum=0.1623,
+        momentum=0.8,
     )
-    opt_func = partial(Adam, wd=0.01, eps=1e-5)
-    learn = Learner(dls, model, loss_func=loss, opt_func=opt_func, metrics=[trends_scorer_multitask_scoring_gpu])
-    return learn
+    return model
