ae83c83599f15d078e3e4bfd31847999aeedcd2a
Nizamuddin <nizamphoenix@gmail.com>
2020-06-28 21:21:44 +1000

    added normalization

--- TRends/read_data.py
+++ TRends/read_data.py
@@ -1,9 +1,12 @@
 import os
 import pandas as pd
 import cudf
+import gc
 def get_train_test(fnc_file,loadings_file,lablels_file):
     '''
     function to get training and test data sets
+    Works with Rapids.ai ONLY
+    
     '''
     path = "../input/trends-assessment-prediction/"
     fnc_df = pd.read_csv(os.path.join(path,fnc_file))
@@ -14,13 +17,25 @@ def get_train_test(fnc_file,loadings_file,lablels_file):
     labels_df["is_train"] = True
     df = df.merge(labels_df, on="Id", how="left")
     test_df = df[df["is_train"] != True].copy()
-    df = df[df["is_train"] == True].copy()
+    train_df = df[df["is_train"] == True].copy()
+    train_df = train_df.drop(['is_train'], axis=1)
+    target_cols = ['age', 'domain1_var1', 'domain1_var2', 'domain2_var1', 'domain2_var2']
+    test_df = test_df.drop(target_cols + ['is_train'], axis=1)
+    features = loading_features + fnc_features 
+    #-----------------Normalizing------------------------
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    train_df[features] = scaler.fit_transform(train_df[features],train_df[target_cols])
+    test_df[features] = scaler.transform(test_df[features])
+    #----------------------------------------------------
     # Giving less importance to FNC features since they are easier to overfit due to high dimensionality.
-    df[fnc_features] *= 1/600
-    test_df[fnc_features] *= 1/600
+    train_df[fnc_features] = train_df[fnc_features].mul(1/600)
+    test_df[fnc_features]  = test_df[fnc_features].mul(1/600) 
     #imputing missing values in targets
     from sklearn.impute import KNNImputer
     imputer = KNNImputer(n_neighbors = 5, weights="distance")
-    df = cudf.DataFrame(pd.DataFrame(imputer.fit_transform(df), columns = list(df.columns)))
-    test_df = cudf.DataFrame(test_df)#necessary for casting to gpu matrix
-    return df,test_df,fnc_features, loading_features
+    train_df = cudf.from_pandas(pd.DataFrame(imputer.fit_transform(train_df), columns = list(train_df.columns)))
+    test_df = cudf.from_pandas(test_df)#necessary for casting to gpu matrix
+    del df
+    gc.collect()
+    return train_df,test_df,features,target_cols
