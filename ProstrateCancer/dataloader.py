2aeb1004a5c8428b5cbf36362fb406c259f2da2e
Nizamuddin <nizamphoenix@gmail.com>
2020-07-02 21:57:43 +1000

    Create dataloader.py

---
+++ ProstrateCancer/dataloader.py
@@ -0,0 +1,58 @@
+def open_image(fn:PathOrStr, div:bool=True, convert_mode:str='RGB', cls:type=Image,after_open:Callable=None)->Image:
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", UserWarning) # EXIF warning from TiffPlugin
+        x = PIL.Image.open(fn).convert(convert_mode)
+    if after_open: 
+        x = after_open(x)
+    x = pil2tensor(x,np.float32)
+    if div: 
+        x.div_(255)
+    return cls(1.0-x) #invert image for zero padding
+
+class MImage(ItemBase):
+    def __init__(self, imgs):
+        self.obj, self.data = \
+          (imgs), [(imgs[i].data - mean[...,None,None])/std[...,None,None] for i in range(len(imgs))]
+    
+    def apply_tfms(self, tfms,*args, **kwargs):
+        for i in range(len(self.obj)):
+            self.obj[i] = self.obj[i].apply_tfms(tfms, *args, **kwargs)
+            self.data[i] = (self.obj[i].data - mean[...,None,None])/std[...,None,None]
+        return self
+    
+    def __repr__(self): return f'{self.__class__.__name__} {img.shape for img in self.obj}'
+    def to_one(self):
+        img = torch.stack(self.data,1)
+        img = img.view(3,-1,N,sz,sz).permute(0,1,3,2,4).contiguous().view(3,-1,sz*N)
+        return Image(1.0 - (mean[...,None,None]+img*std[...,None,None]))
+
+class MImageItemList(ImageList):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def __len__(self)->int: return len(self.items) or 1 
+    
+    def get(self, i):
+        fn = Path(self.items[i])
+        fnames = [Path(str(fn)+'_'+str(i)+'.png')for i in range(N)]
+        imgs = [open_image(fname, convert_mode=self.convert_mode, after_open=self.after_open)
+               for fname in fnames]
+        return MImage(imgs)
+
+    def reconstruct(self, t):
+        return MImage([mean[...,None,None]+_t*std[...,None,None] for _t in t])
+    
+    def show_xys(self, xs, ys, figsize:Tuple[int,int]=(300,50), **kwargs):
+        rows = min(len(xs),8)
+        fig, axs = plt.subplots(rows,1,figsize=figsize)
+        for i, ax in enumerate(axs.flatten() if rows > 1 else [axs]):
+            xs[i].to_one().show(ax=ax, y=ys[i], **kwargs)
+        plt.tight_layout()
+        
+
+#collate function to combine multiple images into one tensor
+def MImage_collate(batch:ItemsList)->Tensor:
+    result = torch.utils.data.dataloader.default_collate(to_data(batch))
+    if isinstance(result[0],list):
+        result = [torch.stack(result[0],1),result[1]]
+    return result
