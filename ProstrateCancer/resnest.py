000a9adb3ffe1e7da20f6e43ee83452ebc798286
Nizamuddin <nizamphoenix@gmail.com>
2020-07-07 21:58:01 +1000

    Update resnest.py

--- ProstrateCancer/resnest.py
+++ ProstrateCancer/resnest.py
@@ -1,19 +1,21 @@
-class Model(nn.Module):
-    def __init__(self, arch='resnext50_32x4d_ssl', n=6, pre=True):
-        super().__init__()
-        
-        #m = torch.hub.load('facebookresearch/semi-supervised-ImageNet1K-models', arch)
-        m = torch.hub.load('zhanghang1989/ResNeSt', 'resnest50', pretrained=True)
 
+class GleasonModel(nn.Module):
+    def __init__(self, arch='resnext50_32x4d_ssl', n=4, pre=True):
+        super().__init__()
+        m = torch.hub.load('zhanghang1989/ResNeSt', 'resnest50', pretrained=True)
+        #m = torch.hub.load('facebookresearch/semi-supervised-ImageNet1K-models', arch)
         self.enc = nn.Sequential(*list(m.children())[:-2])       
         nc = list(m.children())[-1].in_features
         self.head = nn.Sequential(AdaptiveConcatPool2d(),
                                   Flatten(),
-                                  nn.Linear(2*nc,512),
+                                  nn.Linear(2*nc,128),
                                   Mish(),
-                                  nn.BatchNorm1d(512), 
-                                  nn.Dropout(0.5),
-                                  nn.Linear(512,n))
+                                  nn.BatchNorm1d(128), 
+                                  nn.Dropout(0.5))
+        self.prim =  nn.Linear(128,n)
+        self.sec  =  nn.Linear(128,n)
+      
+        
         
     def forward(self, *x):
         shape = x[0].shape
@@ -24,9 +26,10 @@ class Model(nn.Module):
         #x: bs*N x C x 4 x 4
         shape = x.shape
         #concatenate the output for tiles into a single map
-        x = x.view(-1,n,shape[1],shape[2],shape[3]).permute(0,2,1,3,4).contiguous()\
-          .view(-1,shape[1],shape[2]*n,shape[3])
+        x = x.view(-1,n,shape[1],shape[2],shape[3]).permute(0,2,1,3,4).contiguous().view(-1,shape[1],shape[2]*n,shape[3])
         #x: bs x C x N*4 x 4
         x = self.head(x)
         #x: bs x n
-        return x
+        prim_gleason = self.prim(x)
+        sec_gleason  = self.sec(x)
+        return prim_gleason,sec_gleason
