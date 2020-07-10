e5182e6b1d4be765520bf3559ce2eaf81b58bad3
Nizamuddin <nizamphoenix@gmail.com>
2020-07-10 21:55:18 +1000

    Update main.py

--- SIIM-melanoma/main.py
+++ SIIM-melanoma/main.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import torch.nn as nn
 from torch import functional as F
+import albumenations
+
 class SEResNext50_32x4d(nn.Module):
     def __init__(self,pretrained="imagenet"):
         super().__init__()
@@ -17,3 +19,28 @@ class SEResNext50_32x4d(nn.Module):
         x = x.reshape(bs,-1)
         out = self.out(x)
         return out
+
+  def run(fold):
+    training_data_path = ""
+    df = pd.read_csv("train_folds.csv")
+    device = "cuda"
+    epochs=50
+    train_bs = 32
+    valid_bs = 16
+    df_train = df[df.kfold!=fold].reset_index(drop=True)
+    df_valid = df[df.kfold==fold].reset_index(drop=True)
+    mean = (0.485,0.456,0.406)
+    std  = (0.229,0.224,0.225)
+    train_aug = albumenations.Compose(
+        [
+        albumenations.Normalize(mean,std,max_pixel_value=255.0,always_apply=True)
+        ]
+    )
+    valid_aug = albumenations.Compose(
+        [
+        albumenations.Normalize(mean,std,max_pixel_value=255.0,always_apply=True)
+        ]
+    )
+    
+    
+    
