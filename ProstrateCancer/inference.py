4e5802d4c973d29c5ac0496f33715bda16fb0fe6
Nizamuddin <nizamphoenix@gmail.com>
2020-07-13 21:40:57 +1000

    Update inference.py

--- ProstrateCancer/inference.py
+++ ProstrateCancer/inference.py
@@ -72,3 +72,83 @@ class GleasonModel(nn.Module):
         sec_gleason  = self.sec(x)
         preds = [prim_gleason,sec_gleason]
         return preds
+
+    
+models = []
+for path in MODELS:
+    print(path)
+    model = GleasonModel()
+    model_dict = model.state_dict()
+    pretrained_dict = torch.load(path,map_location=torch.device('cpu'))
+    # 1. filter out unnecessary keys
+    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+    # 2. overwrite entries in the existing state dict
+    model_dict.update(pretrained_dict) 
+    # 3. load the new state dict
+    model.load_state_dict(model_dict)
+    model.float()
+    model.eval()
+    model.cuda()
+    models.append(model)
+    
+del pretrained_dict
+
+
+
+def tile(img):
+    shape = img.shape
+    pad0,pad1 = (sz - shape[0]%sz)%sz, (sz - shape[1]%sz)%sz
+    img = np.pad(img,[[pad0//2,pad0-pad0//2],[pad1//2,pad1-pad1//2],[0,0]],constant_values=255)
+    img = img.reshape(img.shape[0]//sz,sz,img.shape[1]//sz,sz,3)
+    img = img.transpose(0,2,1,3,4).reshape(-1,sz,sz,3)
+    if len(img) < N:
+        img = np.pad(img,[[0,N-len(img)],[0,0],[0,0],[0,0]],constant_values=255)
+    idxs = np.argsort(img.reshape(img.shape[0],-1).sum(-1))[:N]
+    img = img[idxs]
+    return img
+
+mean = torch.tensor([1.0-0.90949707, 1.0-0.8188697, 1.0-0.87795304])
+std  = torch.tensor([0.36357649, 0.49984502, 0.40477625])
+
+class PandaDataset(Dataset):
+    def __init__(self, path, test):
+        self.path = path
+        self.names = list(pd.read_csv(test).image_id)
+
+    def __len__(self):
+        return len(self.names)
+
+    def __getitem__(self, idx):
+        name = self.names[idx]
+        img = skimage.io.MultiImage(os.path.join(DATA,name+'.tiff'))[-1]
+        tiles = torch.Tensor(1.0 - tile(img)/255.0)
+        tiles = (tiles - mean)/std
+        return tiles.permute(0,3,1,2), name
+    
+ def get_isup_preds(preds):
+    '''
+    converts gleason scores predicted by the model to isup_grades.
+    returns a torch tensor of isup_preds
+    '''
+    lookup_map  = {(0,0):0,(1,1):1,(1,2):2,(2,1):3,(2,2):4,(3,1):4,(1,3):4,(2,3):5,(3,2):5,(3,3):5}
+    lookup_map2 = {(0,1):1,(0,2):1,(0,3):2,(1,0):1,(2,0):3,(3,0):4}
+    
+    prim_preds = torch.stack([preds[0][0],preds[1][0],preds[2][0],preds[3][0]],dim=1)
+    prim_preds = prim_preds.view(bs,8*len(models),-1).mean(dim=1).argmax(-1).cpu()#shape=2
+    sec_preds  = torch.stack([preds[0][1],preds[1][1],preds[2][1],preds[3][1]],dim=1)
+    sec_preds  = sec_preds.view(bs,8*len(models),-1).mean(dim=1).argmax(-1).cpu()#shape=2
+    
+    temp_preds = torch.cat([prim_preds.view(2,1),sec_preds.view(2,1)],dim=1)
+    temp = []
+    count = 0
+    errors = 0
+    for i in np.array(temp_preds.cpu()):
+        count+=1
+        try:
+            temp.append(lookup_map[tuple(i)])
+        except KeyError:
+            errors+=1
+            temp.append(lookup_map2[tuple(i)])
+    print("count={0},errors={1}".format(count,errors),",correct=",count-errors)
+    isup_preds = torch.tensor(temp,dtype=torch.long,device='cpu')
+    return isup_preds
